867afb74066ed77d6e2d9df5650a498f0988ce6c
liuwenzheng <329541594@qq.com>
2020-08-05 14:35:06 +0800

    修改选择文件bug

--- app/src/main/java/com/moko/scanner/BaseApplication.java
+++ app/src/main/java/com/moko/scanner/BaseApplication.java
@@ -1,8 +1,16 @@
 package com.moko.scanner;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
+import com.moko.scanner.utils.IOUtils;
 import com.moko.support.MokoSupport;
+import com.moko.support.log.LogModule;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 
 import es.dmoral.toasty.Toasty;
 
@@ -12,5 +20,37 @@ public class BaseApplication extends Application {
         super.onCreate();
         MokoSupport.getInstance().init(getApplicationContext());
         Toasty.Config.getInstance().apply();
+        Thread.setDefaultUncaughtExceptionHandler(new BTUncaughtExceptionHandler());
+    }
+
+    public class BTUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+        private static final String LOGTAG = "BTUncaughtExceptionHandler";
+
+        @Override
+        public void uncaughtException(Thread thread, Throwable ex) {
+            // 读取stacktrace信息
+            final Writer result = new StringWriter();
+            final PrintWriter printWriter = new PrintWriter(result);
+            ex.printStackTrace(printWriter);
+            StringBuffer errorReport = new StringBuffer();
+            // 获取packagemanager的实例
+            PackageManager packageManager = getPackageManager();
+            // getPackageName()是你当前类的包名，0代表是获取版本信息
+            PackageInfo packInfo = null;
+            try {
+                packInfo = packageManager.getPackageInfo(getPackageName(), 0);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (packInfo != null) {
+                String version = packInfo.versionName;
+                errorReport.append(version);
+                errorReport.append("\r\n");
+            }
+            errorReport.append(result.toString());
+            IOUtils.setCrashLog(errorReport.toString());
+            LogModule.e("uncaughtException errorReport=" + errorReport);
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
     }
 }
